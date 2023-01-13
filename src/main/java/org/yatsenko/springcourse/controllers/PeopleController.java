057633d65b36eb15cab2dea7267033ce6fb6151d
Eduard <eduardyacenko@gmail.com>
2023-01-13 17:39:46 +0200

    Final commit

--- src/main/java/org/yatsenko/springcourse/controllers/PeopleController.java
+++ src/main/java/org/yatsenko/springcourse/controllers/PeopleController.java
@@ -5,20 +5,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.yatsenko.springcourse.dao.PersonDAO;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
 
+    private final PersonDAO personDAO;
+
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
     @GetMapping()
     public String index(Model model) {
-
-        return null;
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
     }
 
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model) {
-
-        return null;
+        model.addAttribute("person", personDAO.show(id));
+        return "people/show";
     }
 }
--- src/main/java/org/yatsenko/springcourse/dao/PersonDAO.java
+++ src/main/java/org/yatsenko/springcourse/dao/PersonDAO.java
@@ -1,10 +1,12 @@
 package org.yatsenko.springcourse.dao;
 
+import org.springframework.stereotype.Component;
 import org.yatsenko.springcourse.models.Person;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class PersonDAO {
     private static int PEOPLE_COUNT;
     private List<Person> people;
