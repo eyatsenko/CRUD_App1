c7ed567b7caddc33604cfe4902bd15e92d50d72e
Eduard <eduardyacenko@gmail.com>
2023-01-17 17:21:51 +0200

    Added possibility to add new person on main page and buttons for editing each person separately.

--- src/main/java/org/yatsenko/springcourse/dao/PersonDAO.java
+++ src/main/java/org/yatsenko/springcourse/dao/PersonDAO.java
@@ -20,16 +20,26 @@ public class PersonDAO {
         people.add(new Person(++PEOPLE_COUNT, "Katie"));
     }
 
-    public List<Person> index () {
+    public List<Person> index() {
         return people;
     }
 
-    public Person show (int id) {
+    public Person show(int id) {
         return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
     }
 
     public void save(Person person) {
         person.setId(++PEOPLE_COUNT);
         people.add(person);
+    }
+
+    public void update(int id, Person updatedPerson) {
+        Person personToBeUpdated = show(id);
+
+        personToBeUpdated.setName(updatedPerson.getName());
+    }
+
+    public void delete(int id) {
+        people.removeIf(p -> p.getId() == id);
     }
 }
